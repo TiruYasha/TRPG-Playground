941e09b15508af8aadb057e309b60751e4b930b6
TiruYasha <renebuunk1996@gmail.com>
2019-07-07 09:26:53 +0200

    added import for dragmodule

--- angular/src/app/game/map-manager/map-manager.module.ts
+++ angular/src/app/game/map-manager/map-manager.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapManagerComponent } from './map-manager.component';
 import { MatIconModule, MatButtonModule, MatDividerModule, MatListModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatMenuModule } from '@angular/material';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MapListComponent } from './map-list/map-list.component';
 import { LayerManagerComponent } from './layer-manager/layer-manager.component';
 import { CreateMapDialogComponent } from './create-map-dialog/create-map-dialog.component';
@@ -21,7 +22,8 @@ import { MapListItemComponent } from './map-list-item/map-list-item.component';
     ReactiveFormsModule,
     MatInputModule,
     MatFormFieldModule,
-    MatMenuModule
+    MatMenuModule,
+    DragDropModule
   ],
   exports: [
     MapManagerComponent
